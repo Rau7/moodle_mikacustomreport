546ca3164b98decfa7b41f13ffbef9aa82fcc4ba
Alp Toker <tokeralp@gmail.com>
2025-08-11 17:22:01 +0300

    onlyadminandmanager

--- lib.php
+++ lib.php
@@ -9,15 +9,21 @@ defined('MOODLE_INTERNAL') || die();
 function local_mikacustomreport_extend_navigation(global_navigation $navigation) {
     global $CFG;
 
+    // Check if user is logged in and has the required capability
     if (isloggedin() && !isguestuser()) {
-        // Add to custom menu items
-        if (stripos($CFG->custommenuitems, "/local/mikacustomreport/") === false) {
-            $nodes = explode("\n", $CFG->custommenuitems);
-            $node = get_string('pluginname', 'local_mikacustomreport');
-            $node .= "|";
-            $node .= "/local/mikacustomreport/index.php";
-            array_push($nodes, $node);
-            $CFG->custommenuitems = implode("\n", $nodes);
+        $context = context_system::instance();
+        
+        // Only show to users with the view capability (managers/admins)
+        if (has_capability('local/mikacustomreport:view', $context)) {
+            // Add to custom menu items
+            if (stripos($CFG->custommenuitems, "/local/mikacustomreport/") === false) {
+                $nodes = explode("\n", $CFG->custommenuitems);
+                $node = get_string('pluginname', 'local_mikacustomreport');
+                $node .= "|";
+                $node .= "/local/mikacustomreport/index.php";
+                array_push($nodes, $node);
+                $CFG->custommenuitems = implode("\n", $nodes);
+            }
         }
     }
 }
